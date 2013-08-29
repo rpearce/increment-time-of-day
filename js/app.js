c4eb8d464de154e9b15e619221c620b744b5c819
Robert Pearce <robertwaltonpearce@gmail.com>
2013-08-29 03:20:25 -0400

    modulated all relevant functions into separate namespaced file

--- js/app.js
+++ js/app.js
@@ -1,90 +1,20 @@
 ;(function() {
+  // Set any defaults you want to be set first
+  var defaults = ['Any', 'Early (4a-8a)', 'Morning (8a-12p)', 'Afternoon (12p-5p)', 'Evening (5p-9p)', 'Night (9p-12a)'];
+
   /**
-    *
-    * Increment Time of Day
-    * by Robert Pearce
+    * Call the getTimes method,
+    * passing the defaults, as well
+    * as an amount to increment each
+    * hour by.
     *
     */
-
-  var padNumber = function(n) {
-    // Prepend 0 if number is less than 10
-    return (n < 10) ? '0' + n : n;
-  };
-
-  var setDefaultTimes = function(defaults) {
-    try {
-      if(Array.isArray(defaults)) {
-        return defaults;
-      } else {
-          throw {
-            name: 'DefaultsTypeError',
-            message: 'Defaults must be of type Array',
-            extra: ''
-          }
-      }
-    } catch (e) {
-        alert(e.message);
-    }
-  };
-
-  var setMinuteIncrement = function(incrementMinutesBy) {
-    try {
-      if(typeof incrementMinutesBy === 'number' && incrementMinutesBy < 60) {
-        return incrementMinutesBy;
-      } else {
-          throw {
-            name: 'MinuteIncrementError',
-            message: 'incrementMinutesBy must be a number and must be less than 60',
-            extra: ''
-          }
-      }
-    } catch (e) {
-        alert(e.message);
-    }
-  };
-
-  var getTimes = function(opts) {
-    var defaults = setDefaultTimes(opts.defaults),
-        incrementMinutesBy = setMinuteIncrement(opts.incrementMinutesBy),
-        periods = ['am', 'pm'],
-        hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-        times = [];
-
-    /**
-      *
-      * Iterate through every minute increment
-      * of every hour
-      * that falls within "am" or "pm"
-      *
-      */
-    for(var p = 0; p < periods.length; p += 1) {
-      for(var h = 0; h < hours.length; h += 1) {
-        for(var i = 0; i < 60; i += incrementMinutesBy) {
-          times.push(hours[h].toString() + ':' + padNumber(i).toString() + periods[p]);
-        }
-      }
-    }
-
-    return defaults.concat(times);
-  };
-
-  var generateOptionsHTML = function(times) {
-    var html = [];
-    for(var i = 0; i < times.length; i += 1) {
-      var time = times[i],
-          option = document.createElement('option');
-      option.value = time;
-      option.text = time;
-      html.push(option);
-    }
-
-    return html;
-  };
+  var times = ITOD.getTimes({ defaults: defaults, incrementMinutesBy: 15 });
 
   var renderSelectOptions = function(times) {
     // Generate options and append to select element
 
-    var options = generateOptionsHTML(times),
+    var options = ITOD.generateOptionsHTML(times),
         selectElem = document.querySelectorAll('.times')[0];
     for(var i = 0; i < options.length; i += 1) {
       selectElem.appendChild(options[i]);
@@ -92,18 +22,6 @@
 
     return;
   };
-
-  // Set any defaults you want to be first
-  var defaults = ['Any', 'Early (4a-8a)', 'Morning (8a-12p)', 'Afternoon (12p-5p)', 'Evening (5p-9p)', 'Night (9p-12a)'];
-
-  /**
-    * Call the getTimes function,
-    * passing the defaults, as well
-    * as an amount to increment each
-    * hour by.
-    *
-    */
-  var times = getTimes({ defaults: defaults, incrementMinutesBy: 15 });
 
   return renderSelectOptions(times);
 })();
---
+++ js/itod.js
@@ -0,0 +1,83 @@
+/**
+  *
+  * Increment Time of Day
+  * by Robert Pearce
+  *
+  */
+
+var ITOD = {
+  padNumber: function(n) {
+    // Prepend 0 if number is less than 10
+    return (n < 10) ? '0' + n : n;
+  },
+
+  setDefaultTimes: function(defaults) {
+    try {
+      if(Array.isArray(defaults)) {
+        return defaults;
+      } else {
+          throw {
+            name: 'DefaultsTypeError',
+            message: 'Defaults must be of type Array',
+            extra: ''
+          }
+      }
+    } catch (e) {
+        alert(e.message);
+    }
+  },
+
+  setMinuteIncrement: function(incrementMinutesBy) {
+    try {
+      if(typeof incrementMinutesBy === 'number' && incrementMinutesBy < 60) {
+        return incrementMinutesBy;
+      } else {
+          throw {
+            name: 'MinuteIncrementError',
+            message: 'incrementMinutesBy must be a number and must be less than 60',
+            extra: ''
+          }
+      }
+    } catch (e) {
+        alert(e.message);
+    }
+  },
+
+  getTimes: function(opts) {
+    var defaults = this.setDefaultTimes(opts.defaults),
+        incrementMinutesBy = this.setMinuteIncrement(opts.incrementMinutesBy),
+        periods = ['am', 'pm'],
+        hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        times = [];
+
+    /**
+      *
+      * Iterate through every minute increment
+      * of every hour
+      * that falls within "am" or "pm"
+      *
+      */
+    for(var p = 0; p < periods.length; p += 1) {
+      for(var h = 0; h < hours.length; h += 1) {
+        for(var i = 0; i < 60; i += incrementMinutesBy) {
+          times.push(hours[h].toString() + ':' + this.padNumber(i).toString() + periods[p]);
+        }
+      }
+    }
+
+    return defaults.concat(times);
+  },
+
+  generateOptionsHTML: function(times) {
+    var html = [];
+    for(var i = 0; i < times.length; i += 1) {
+      var time = times[i],
+          option = document.createElement('option');
+      option.value = time;
+      option.text = time;
+      html.push(option);
+    }
+
+    return html;
+  }
+};
